24da5b44901659380342673448f8c13b546d5f98
sardaceylan <arda_ceylanrnwr@yahoo.com>
2022-04-06 04:10:43 +0300

    Http Request

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from "react";
 import Navbar from "./Navbar";
 import Users from "./Users";
+import axios from "axios";
 
 export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+    };
+  }
+  componentDidMount() {
+    axios
+      .get("https://api.github.com/users")
+      .then((res) => this.setState({ users: res.data }));
+  }
   render() {
     return (
       <div>
         <Navbar />
-        <Users />
+        <Users users={this.state.users}/>
       </div>
     );
   }
--- src/components/Users.js
+++ src/components/Users.js
@@ -2,44 +2,13 @@ import React, { Component } from "react";
 import User from "./User";
 
 export class Users extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      users: [
-        {
-          login: "mojombo",
-          id: 1,
-          avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-          html_url: "https://github.com/mojombo",
-        },
-        {
-          login: "defunkt",
-          id: 2,
-          avatar_url: "https://avatars.githubusercontent.com/u/2?v=4",
-          html_url: "https://github.com/defunkt",
-        },
-        {
-          login: "pjhyett",
-          id: 3,
-          avatar_url: "https://avatars.githubusercontent.com/u/3?v=4",
-          html_url: "https://github.com/pjhyett",
-        },
-        {
-          login: "pjhyett",
-          id: 3,
-          avatar_url: "https://avatars.githubusercontent.com/u/3?v=4",
-          html_url: "https://github.com/pjhyett",
-        },
-      ],
-    };
-  }
   render() {
-    const { users } = this.state;
+    const { users } = this.props;
     return (
       <div className="container mt-3">
         <div className="row">
           {users.map((user) => (
-            <User user={user} />
+            <User user={user} key={user.id} />
           ))}
         </div>
       </div>
